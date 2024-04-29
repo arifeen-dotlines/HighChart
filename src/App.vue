93a017357bf6032bcd566390bf8a8e553216194f
Shamsul009 <82135940+Shamsul009@users.noreply.github.com>
2024-04-29 17:37:39 +0600

    highchart

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,63 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld /> -->
+    <LineChart/>
+     <!-- <BarChart/> -->
+    <!-- <AreaChart/> -->
+    <!-- <ThreeDChart/> -->
+    <!-- <BubbleChart></BubbleChart> -->
+    <!-- <Combinations/> -->
+    <!-- <CombinationsChart/> -->
+    <!-- <DynamicChart/> -->
+    <!-- <GaugesChart/> -->
+    <!-- <HeatTreeChart/> -->
+    <!-- <LoadModuleChart/> -->
+    <!-- <PieChart/> -->
+    <!-- <ResponsiveChart/> -->
+    <!-- <ScatterPlotChart/> -->
+    
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import ScatterPlotChartVue from './components/ScatterPlotChart.vue'
+// import ResponsiveChartVue from './components/ResponsiveChart.vue'
+// import LoadModuleChartVue from './components/LoadModuleChart.vue'
+// import HeatTreeMap from './assets/HeatTreeMap'
+// import HelloWorld from './components/HelloWorld.vue'
+import LineChart from './components/LineChart.vue'
+// import BarChart from './components/BarChart.vue'
+// import AreaChart from './components/AreaChart.vue'
+// import ThreeDChart from './components/ThreeDChart.vue'
+// import BubbleChart from './components/BubbleChart.vue'
+// import CombinationsChart from './components/CombinationsChart.vue'
+// import DynamicChart from './components/DynamicChart.vue'
+// import GaugesChart from './components/GaugesChart.vue'
+// import HeatTreeChart from './components/HeatTreeChart.vue'
+// import LoadModuleChart from './components/LoadModuleChart.vue'
+// import PieChart from './components/PieChart.vue'
+// import ResponsiveChart from './components/ResponsiveChart.vue'
+// import ScatterPlotChart from './components/ScatterPlotChart.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    LineChart,
+    // BarChart
+    // AreaChart
+    // ThreeDChart
+    // BubbleChart
+    // CombinationsChart
+    // DynamicChart
+    // GaugesChart
+    // HeatTreeChart
+    // LoadModuleChart
+    // PieChart
+    // ResponsiveChart
+    // ScatterPlotChart
+    
   }
 }
 </script>
